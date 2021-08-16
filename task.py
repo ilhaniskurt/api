30479263f3e02658460a5d97c3c85a9a241c2dff
ilhaniskurt <ilhanyavuz.02@hotmail.com>
2021-08-16 17:02:21 +0300

    added folder check

--- task.py
+++ task.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 @app.post("/file")
 def check_file(file: UploadFile = File(...)):
-  
+  # Check Folder
+  if not os.path.exists("./files"):
+    os.mkdir("./files")
+    
   # Download the file from user
   with open("./files/"+file.filename, 'wb') as buffer:
     shutil.copyfileobj(file.file, buffer)
