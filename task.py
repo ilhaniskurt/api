4636311700dfe898ac40059bd2d6786fa6dee9a6
ilhaniskurt <ilhanyavuz.02@hotmail.com>
2021-08-16 23:41:58 +0300

    cleaned code

--- task.py
+++ task.py
@@ -13,21 +13,28 @@ def check_file(file: UploadFile = File(...)):
     os.mkdir("./files")
 
   # Download the file from user
-  with open("./files/"+file.filename, 'wb') as buffer:
+  filepath = "./files/" + file.filename
+  tmpfilepath = "./file/tmp_" + file.filename[:-4]
+
+  with open(filepath, 'wb') as buffer:
     shutil.copyfileobj(file.file, buffer)
   
   # .apk check
   if file.filename.lower().endswith('.apk'):
     # tmp copy
-    if not os.path.exists("./files/tmp_"+file.filename[:-4]):
-      os.mkdir("./files/tmp_"+file.filename[:-4])
-    shutil.copyfile("./files/"+file.filename, "./files/tmp_"+file.filename[:-4]+"/"+file.filename[:-4]+".zip")
-    
-    #patoolib.extract_archive("./files/tmp_"+file.filename[:-4]+"/"+file.filename[:-4]+".rar", outdir="./files/tmp_"+file.filename[:-4])
-
-    # Unzip with ZipFile 
-    #with ZipFile("./files/tmp_"+file.filename[:-4]+"/"+file.filename[:-4]+".zip", 'r') as zip_ref:
-      #zip_ref.extractall("./files/tmp_"+file.filename[:-4])
+    if not os.path.exists(tmpfilepath):
+      os.mkdir(tmpfilepath)
+    # alter format
+    shutil.copyfile(filepath, tmpfilepath +"/" + file.filename[:-4]+".zip")
     return {"filename":file.filename,"extension":".apk"}
-  
   return {"filename":file.filename}
+
+
+
+
+
+
+#patoolib.extract_archive("./files/tmp_"+file.filename[:-4]+"/"+file.filename[:-4]+".rar", outdir="./files/tmp_"+file.filename[:-4])
+  # Unzip with ZipFile 
+  #with ZipFile("./files/tmp_"+file.filename[:-4]+"/"+file.filename[:-4]+".zip", 'r') as zip_ref:
+   #zip_ref.extractall("./files/tmp_"+file.filename[:-4])
