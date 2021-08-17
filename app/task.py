d3187a6401263e0e24ed9c7f7a36013e8d166ae9
ilhaniskurt <ilhanyavuz.02@hotmail.com>
2021-08-17 14:16:20 +0300

    reorganized

--- app/task.py
+++ app/task.py
@@ -12,33 +12,36 @@ def index():
 
 @app.post("/file")
 def check_file(file: UploadFile = File(...)):
-  # Check Folder
-  if not os.path.exists("./files"):
-    os.mkdir("./files")
-
-  # Download the file from user
+  # Path variables
   filepath = "./files/" + file.filename
   tmpdirpath = "./files/tmp_" + file.filename[:-4] + "/"
-
+  # Check Files Folder
+  if not os.path.exists("./files"):
+    os.mkdir("./files")
+  # Download the file from user
   with open(filepath, 'wb') as buffer:
     shutil.copyfileobj(file.file, buffer)
   
+  format = formatCheck(file, filepath, tmpdirpath)
+  return format
+  
+def formatCheck(file, filepath, tmpdirpath):
+  # Check Temp Path
+  if not os.path.exists(tmpdirpath):
+      os.mkdir(tmpdirpath)  
   # .apk check
   if file.filename.lower().endswith('.apk'):
-    # tmp copy
-    if not os.path.exists(tmpdirpath):
-      os.mkdir(tmpdirpath)
-    # alter format
+    # alter format to .jar
     shutil.copyfile(filepath, tmpdirpath + file.filename[:-4]+".jar")
+    # Unzip jar
     with ZipFile(tmpdirpath + file.filename[:-4] + ".jar", 'r') as zip_ref:
       zip_ref.extractall(tmpdirpath)
-
     # scan for 'classes.dex'
     if os.path.exists(tmpdirpath + "classes.dex"):
       # remove tmp
       shutil.rmtree(tmpdirpath)
-      return {"filename":file.filename,"extension":".apk","found":"classes.dex"}
+      return {"filename":file.filename,"format":"APK","found":"classes.dex"}
     # remove tmp
     shutil.rmtree(tmpdirpath)
-    return {"filename":file.filename,"extension":".apk"}
-  return {"filename":file.filename}
+    return {"filename":file.filename,"format":"JAR"}
+  return {"filename":file.filename,"format":"not supported"}
